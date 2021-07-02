6e8d0e0d76624c9dd32a4d2a1d463e9ff9f6fae6
a.sivak <riziy_bes@mail.ru>
2021-07-03 01:45:22 +0300

    add general settings

--- wp-content/themes/food/functions.php
+++ wp-content/themes/food/functions.php
@@ -314,3 +314,23 @@ function restaurant_register_taxonomy() {
     );
 }
 add_action( 'init', 'restaurant_register_taxonomy' );
+
+if( function_exists('acf_add_options_page') ) {
+
+    acf_add_options_page(array(
+        'page_title' 	=> 'Theme General Settings',
+        'menu_title'	=> 'General Settings',
+        'menu_slug' 	=> 'theme-general-settings',
+        'capability'	=> 'edit_posts',
+        'redirect'		=> false
+    ));
+
+    acf_add_options_page(array(
+        'page_title' 	=> 'Footer',
+        'menu_title'	=> 'Footer',
+        'menu_slug' 	=> 'footer-settings',
+        'capability'	=> 'edit_posts',
+        'redirect'		=> false
+    ));
+
+}
