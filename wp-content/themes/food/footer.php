74e1c567a26aa562609b437fc6d425ec0afbe3fa
a.sivak <riziy_bes@mail.ru>
2021-07-03 01:46:17 +0300

    edit footer. add main info

--- wp-content/themes/food/footer.php
+++ wp-content/themes/food/footer.php
@@ -12,21 +12,13 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'food' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'food' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'food' ), 'food', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+		<p>About us</p>
+    <p><?= get_field('about_us', 'option') ?></p>
+    <p>About delivery</p>
+    <p><?= get_field('about_delivery', 'option') ?></p>
+    <p></p>
+	</footer>
+</div>
 
 <?php wp_footer(); ?>
 
