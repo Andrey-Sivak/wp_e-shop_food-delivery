1fd30961ecf71a744db0cc614d22c3ca52160f83
a.sivak <riziy_bes@mail.ru>
2021-07-05 09:33:06 +0300

    edit footer and styling it

--- wp-content/themes/food/footer.php
+++ wp-content/themes/food/footer.php
@@ -11,12 +11,20 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<p>About us</p>
-    <p><?= get_field('about_us', 'option') ?></p>
-    <p>About delivery</p>
-    <p><?= get_field('about_delivery', 'option') ?></p>
-    <p></p>
+	<footer id="colophon"
+          class="footer"
+          style="background-color: <?= get_field('footer_background', 'option'); ?>;">
+    <div class="container">
+      <div class="footer__text-block">
+        <p class="footer__caption">About us</p>
+        <p class="footer__text"><?= get_field('about_us', 'option') ?></p>
+      </div>
+      <div class="footer__text-block">
+        <p class="footer__caption">About delivery</p>
+        <p class="footer__text"><?= get_field('about_delivery', 'option') ?></p>
+      </div>
+    </div>
+    <p class="footer__copyright"><span class="footer__copyright_bold">&copy; biglunch.online 2019</span> contact us <a                href="<?= get_field('phone', 'option') ?>" class="footer__copyright_link">WhatsApp</a></p>
 	</footer>
 </div>
 
