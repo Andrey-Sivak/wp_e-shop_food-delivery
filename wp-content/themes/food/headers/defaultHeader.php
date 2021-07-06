e9c50e07541f01baba3fbc3b12aa1073e0225272
a.sivak <riziy_bes@mail.ru>
2021-07-06 23:37:32 +0300

    wrapped main banner text on default header

--- wp-content/themes/food/headers/defaultHeader.php
+++ wp-content/themes/food/headers/defaultHeader.php
@@ -3,7 +3,7 @@
 <div class="container">
   <div class="header__wrap">
     <div class="header__text">
-      <p class="header__text_main"><?= get_field('main_banner_text'); ?></p>
+      <div class="header__text_main"><?= get_field('main_banner_text'); ?></div>
       <p class="header__text_simple"><?= get_field('text'); ?></p>
     </div>
     <div class="header__img">
